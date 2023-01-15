6385eb1232f0016d0a9c0f43c52d1d82dc3f5425
vkco <stasmakii@gmail.com>
2023-01-15 22:47:21 +0300

    test commit: commite file 'calculator.php'

---
+++ calculator.php
@@ -0,0 +1,42 @@
+<DOCTUOE html>
+  <html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <mate name="viewport" content="width=devise-width,initial-scale=1.0">
+    <title>Calculator</title>
+    <link rel="stylesheet" href="css\style_calc.css"
+    </head>
+    <body>
+      <li><a href="/index.php">Exit</a></li>
+      <li><a href="/calculator.php">Reset</a></li>
+      <li></li>
+
+      <div class="calc"> <!-- "calc"-->
+        <div class="calc-screen"> <!-- "calc-screen"-->
+        <p>A long, long time ago... </p>
+        </div>
+        <div class="buttons">
+          <div class="btn">ac</div>
+          <div class="btn">+/-</div>
+          <div class="btn">%</div>
+          <div class="btn">/</div>
+          <div class="btn">7</div>
+          <div class="btn">8</div>
+          <div class="btn">9</div>
+          <div class="btn">x</div>
+          <div class="btn">4</div>
+          <div class="btn">5</div>
+          <div class="btn">6</div>
+          <div class="btn">-</div>
+          <div class="btn">1</div>
+          <div class="btn">2</div>
+          <div class="btn">3</div>
+          <div class="btn">+</div>
+          <div class="btn">0</div>
+          <div class="btn">-</div>
+          <div class="btn">=</div>
+  </div>
+      </div>
+    </body>
+    </html>
