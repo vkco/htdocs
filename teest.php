f8c116d09839a3272fc2bdcb610403222f0afda0
vkco <stasmakii@gmail.com>
2023-01-21 20:01:33 +0300

    cleaning_1

--- teest.php
+++ teest.php
@@ -7,13 +7,13 @@
 </head>
 <body>
   <ul>
-  <li style="--i:9"><a href="/index.php">Home</a></li>
-  <li style="--i:6"><a href="/calcylator.php">calcylator</a></li>
-  <li style="--i:5"><a href="/jsl.php">JS_Lessons_2</a></li>
-  <li style="--i:4"><a href="/youtube.php">Test_1.</a></li>
-  <li style="--i:3"><a href="/test_2.php">Test_2.</a></li>
-  <li style="--i:2"><a href="/old_reg.php">old reg</a></li>
-  <li style="--i:1"><a href="/test_4.php">JS_Lessons</a></li>
+  <li style="--i:8"><a href="/index.php">Home</a></li>
+  <li style="--i:7"><a href="/calcylator.php">calcylator</a></li>
+  <li style="--i:6"><a href="/jsl.php">JS_Lessons_2</a></li>
+  <li style="--i:5"><a href="/JS_Lessons_0.html">JS_Lessons_0</a></li>
+  <li style="--i:3"><a href="/youtube.php">Test_1.</a></li>
+  <li style="--i:2"><a href="/test_2.php">Test_2.</a></li>
+  <li style="--i:1"><a href="/old_reg.php">old reg</a></li>
   </ul>
 </body>
 </html>
