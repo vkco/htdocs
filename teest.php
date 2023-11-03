898ac61a3a6fd12687af842d54c39b897b0c6079
vkco <stasmakii@gmail.com>
2023-11-03 21:56:45 +0300

    optimization

--- teest.php
+++ teest.php
@@ -7,8 +7,7 @@
 </head>
 <body>
   <ul>
-  <li style="--i:8"><a href="/index.php">Home</a></li>
-  <li style="--i:7"><a href="/calcylator.php">calcylator</a></li>
+  <li style="--i:7"><a href="/index.php">Home</a></li>
   <li style="--i:6"><a href="/jsl.php">JS_Lessons_2</a></li>
   <li style="--i:5"><a href="/JS_Lessons_0.html">JS_Lessons_0</a></li>
   <li style="--i:3"><a href="/youtube.php">Test_1.</a></li>
